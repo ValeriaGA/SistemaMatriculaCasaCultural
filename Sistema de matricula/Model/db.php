dce2cb4f69701c2e85b7e06a98e65d15b9674fa5
ValeriaGA <valeriaga1000@gmail.com>
2019-05-21 13:25:29 -0600

    Eliminacion de paginas basura, templates

--- Sistema de matricula/Model/db.php
+++ Sistema de matricula/Model/db.php
@@ -4,7 +4,12 @@
 
 //Esta funcion devuelve la variable con, es el resultado de intentar conectarse a la base de datos.
 function conectar(){
-        $con = mysqli_connect('127.0.0.1', 'root', '', 'casacult_casa_cultural'); 
+        $servername = 'localhost';
+        $username = 'root';
+        $password = 'root';
+        $databasename = 'casacult_casa_cultural';
+            
+        $con = mysqli_connect($servername,$username, $password, $databasename); 
         mysqli_set_charset($con,"utf8");
         return $con;
     }
--- Sistema de matricula/php.php
+++ Sistema de matricula/php.php
@@ -1,1 +1,20 @@
-<?php phpinfo(); ?>
+<?php 
+echo "Hola<br>";
+$servername = 'casaculturalamon.com';
+$username = 'casacult_admin';
+$password = 'ABCDEFG12345';
+$databasename = 'casacult_casa_cultural';
+
+$con = mysqli_connect($servername,$username, $password, $databasename); 
+
+
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_set_charset($con,"utf8");
+return $con;
+
+
+?>
